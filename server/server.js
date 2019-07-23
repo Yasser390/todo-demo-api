5fce44d66405e0bdb663c595d1295da30ef8a8e9
Yasser390 <yasser.zahran.390@gmail.com>
2019-07-24 00:56:24 +0200

    Added PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const {ObjectID} = require('mongodb');  
 var {mongoose} = require('./db/mongoose');
@@ -61,6 +62,30 @@ app.delete('/todos/:id',(req,res)=>{
         res.status(400).send('there is an error in request');
         //console.log('no connection');
     });
+});
+
+app.patch('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    var body =_.pick(req.body,['text','completed']);
+    
+    if(!ObjectID.isValid(id)){
+        //console.log('id is not valid');
+        return res.status(404).send('id is not valid');
+    }
+
+    if (_.isBoolean(body.completed)&&body.completed){
+        body.completedAt = new Date().getTime();
+    } else{
+        body.completed = false;
+        body.completedAt = null;    
+    }
+
+    Todo.findByIdAndUpdate(id,{$set:body},{new: true}).then((todo)=>{
+        if(!todo) return res.status(404).send('cannot find todo');
+        res.send({todo});
+    }).catch((e) =>{res.status(400).send()});
+
+    
 });
 
 app.listen(port,() =>{
