3f4206ab6a65184bf7c9007d7fc059830448b997
Yasser390 <yasser.zahran.390@gmail.com>
2019-07-28 00:37:25 +0200

    Hash user password before save

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const fs = require('fs');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
     email:{
@@ -73,6 +74,21 @@ UserSchema.statics.findByToken = function(token){ //3amla zy el static method fe
         'tokens.access': 'auth'
     });
 };
+
+UserSchema.pre('save',function (next){
+    var user = this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(10,(err,salt)=>{
+            bcrypt.hash(user.password,salt,(err,hash)=>{
+                //console.log(hash);
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
 var User = mongoose.model('User',UserSchema);
 
